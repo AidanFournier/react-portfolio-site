cd465c2a9fbf833fa9e60ba6e9963488f287fcd0
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-22 23:52:35 +0900

    Added icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { AiFillHome } from 'react-icons/ai'
+import { RiUser3Fill } from 'react-icons/ri'
+import { FaBook } from 'react-icons/fa'
+import { FaFolder } from 'react-icons/fa'
+import { AiFillMessage } from 'react-icons/ai'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiFillHome /></a>
+      <a href="#about"><RiUser3Fill /></a>
+      <a href="#experience"><FaBook /></a>
+      <a href="#portfolio"><FaFolder /></a>
+      <a href="#contact"><AiFillMessage /></a>
+    </nav>
   )
 }
 
