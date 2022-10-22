33182515eeebe7ed208981d8104b82e49c8b68f9
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-22 14:03:52 +0900

    Added header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import CTA from './CTA'
+import MyPic from '../assets/Profile Pic - Transparent.png'
 
 const Header = () => {
   return (
@@ -7,6 +9,13 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Aidan Fournier</h1>
         <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={MyPic} alt="" />
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
   )
