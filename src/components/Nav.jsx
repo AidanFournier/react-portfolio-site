f3c4bfebf4f008f56bdf0874cc10d2a31bca106f
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-23 21:15:23 +0900

    Added useState to toggle nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
+import { useState } from 'react'
 import { AiFillHome } from 'react-icons/ai'
 import { RiUser3Fill } from 'react-icons/ri'
 import { FaBook } from 'react-icons/fa'
 import { FaFolder } from 'react-icons/fa'
 import { AiFillMessage } from 'react-icons/ai'
 
+
 const Nav = () => {
+  const [ activeNav, setActiveNav ] = useState('#home')
+
   return (
     <nav>
-      <a href="#home" className="active"><AiFillHome /></a>
-      <a href="#about"><RiUser3Fill /></a>
-      <a href="#experience"><FaBook /></a>
-      <a href="#portfolio"><FaFolder /></a>
-      <a href="#contact"><AiFillMessage /></a>
+      <a href="#home"
+        onClick={() => setActiveNav('#home')}
+        className={activeNav === '#home' ? 'active' : ''}>
+          <AiFillHome />
+      </a>
+      <a href="#about" 
+        onClick={() => setActiveNav('#about')} 
+        className={activeNav === '#about' ? 'active' : ''}>
+          <RiUser3Fill />
+      </a>
+      <a href="#experience" 
+        onClick={() => setActiveNav('#experience')}
+        className={activeNav === '#experience' ? 'active' : ''}>
+          <FaBook />
+      </a>
+      <a href="#portfolio" 
+        onClick={() => setActiveNav('#portfolio')}
+        className={activeNav === '#portfolio' ? 'active' : ''}>
+          <FaFolder />
+      </a>
+      <a href="#contact" 
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}>
+          <AiFillMessage />
+      </a>
     </nav>
   )
 }
