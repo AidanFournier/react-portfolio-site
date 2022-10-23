8fedd686ed02e953d090f17d9b308999d4ee7fe6
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-23 23:46:04 +0900

    Created about page structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="about__content">
+
+
+      </div>
+    </section>
   )
 }
 
