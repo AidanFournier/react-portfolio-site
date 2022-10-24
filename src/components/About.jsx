ec0061343ee437924c534ea2f16d6e5db977dc22
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-24 22:42:27 +0900

    Added styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Me from '../assets/me-about.jpg'
-import { GrPersonalComputer } from 'react-icons/gr'
+import { RiCodeSSlashFill } from 'react-icons/ri'
 import { BiGitBranch } from 'react-icons/bi'
 import { GoBrowser } from 'react-icons/go'
 
@@ -16,27 +16,27 @@ const About = () => {
             <img src={Me} alt="Me" />
           </div>
         </div>
-      </div>
 
-      <div className="about__content">
-        <div className="about__cards">
-          <article className="about__card">
-            <GrPersonalComputer className="about__icon"/>
-            <h5>Experience</h5>
-            <small>3+ Years Coding</small>
-          </article>
+        <div  iv className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <RiCodeSSlashFill className="about__icon"/>
+              <h5>Experience</h5>
+              <small>3+ Years Coding</small>
+            </article>
 
-          <article className="about__card">
-            <BiGitBranch className="about__icon"/>
-            <h5>Projects</h5>
-            <small>40+ repos</small>
-          </article>
+            <article className="about__card">
+              <BiGitBranch className="about__icon"/>
+              <h5>Projects</h5>
+              <small>40+ repos</small>
+            </article>
 
-          <article className="about__card">
-            <GoBrowser className="about__icon"/>
-            <h5>Products</h5>
-            <small>4 Live Sites</small>
-          </article>
+            <article className="about__card">
+              <GoBrowser className="about__icon"/>
+              <h5>Products</h5>
+              <small>4 Live Sites</small>
+            </article>
+          </div>
 
           <p>
             I have a proven track record of writing clean code and designing websites with an eye for aesthetics. I am experienced in building complete applications from the ground up. From my years as a teacher I have nurtured my skills in patience, organization, and mentoring - all of which are invaluable as a member of any team. 
