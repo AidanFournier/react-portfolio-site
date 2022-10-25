f939fccd045e22a96ee48ebf94bac9d9890fb5f7
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-25 21:26:16 +0900

    Finished jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import { BsFillPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>My Skills and</h5>
+      <h2>Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>HTML5</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>CSS3</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>JavaScript (ES6)</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>TypeScript</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>React</h4>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>Ruby on Rails</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>Node.js</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>PostgreSQL</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>MongoDB</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
