1ffeae3e619c4232b9e5bd06fc5cbfa120d571d2
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-26 21:54:44 +0900

    Created jsx structure

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import JusticeImg from '../assets/JusticeMockup.png'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Projects</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={JusticeImg} alt="Justice mockup" />
+          </div>
+          <h3>Justice</h3>
+          <a href="https://github.com/AidanFournier/justice" className="btn" target="_blank" rel="noreferrer">GitHub</a>
+          <a href="https://www.wearejustice.life/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={JusticeImg} alt="Justice mockup" />
+          </div>
+          <h3>Justice</h3>
+          <a href="https://github.com/AidanFournier/justice" className="btn" target="_blank" rel="noreferrer">GitHub</a>
+          <a href="https://www.wearejustice.life/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={JusticeImg} alt="Justice mockup" />
+          </div>
+          <h3>Justice</h3>
+          <a href="https://github.com/AidanFournier/justice" className="btn" target="_blank" rel="noreferrer">GitHub</a>
+          <a href="https://www.wearejustice.life/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={JusticeImg} alt="Justice mockup" />
+          </div>
+          <h3>Justice</h3>
+          <a href="https://github.com/AidanFournier/justice" className="btn" target="_blank" rel="noreferrer">GitHub</a>
+          <a href="https://www.wearejustice.life/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
