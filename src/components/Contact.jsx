b0a3003b6d03fcf92834794c882697e8c891966b
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-30 22:47:51 +0900

    Added content to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { MdOutlineMail } from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>aidan.douglas.fournier@gmail.com</h5>
+            <a href="mailto:aidan.douglas.fournier@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+        </div>
+
+        <form action="">
+          <input type="text" name="name" placeholder="Your full name" required />
+          <input type="email" name="email" placeholder="Your email address" required />
+          <textarea name="message" rows="7" placeholder="Your message" required ></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
