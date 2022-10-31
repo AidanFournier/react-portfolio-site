2f5bb9e85d3047842eeb1e934f72c5a9342c90aa
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-10-31 22:05:22 +0900

    Added links and copyright to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+import { BsTwitter } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#header" className="footer__logo">Aidan</a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/aidanfournier/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+        <a href="https://github.com/AidanFournier" target="_blank" rel="noreferrer"><BsGithub /></a>
+        <a href="https://twitter.com/aidoufou" target="_blank" rel="noreferrer"><BsTwitter /></a> 
+      </div>
+    </footer>
   )
 }
 
