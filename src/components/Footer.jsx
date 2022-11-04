65e55ed10a5214aec69de6421d870727636ac482
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-11-05 02:36:52 +0900

    Cleaned up style of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import { BsLinkedin } from 'react-icons/bs'
-import { BsGithub } from 'react-icons/bs'
-import { BsTwitter } from 'react-icons/bs'
+import { RiLinkedinLine } from 'react-icons/ri'
+import { FiGithub } from 'react-icons/fi'
+import { FiTwitter } from 'react-icons/fi'
+import Logo from '../assets/af-logo.png'
+
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#header" className="footer__logo">Aidan</a>
-      <ul className="permalinks">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-      </ul>
-
+      <a href="#header" className="footer__logo-link">
+        <img src={Logo} alt="Brand logo" className="footer__logo"/>
+      </a>
       <div className="footer__socials">
-        <a href="https://www.linkedin.com/in/aidanfournier/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
-        <a href="https://github.com/AidanFournier" target="_blank" rel="noreferrer"><BsGithub /></a>
-        <a href="https://twitter.com/aidoufou" target="_blank" rel="noreferrer"><BsTwitter /></a> 
+        <a href="https://www.linkedin.com/in/aidanfournier/" target="_blank" rel="noreferrer"><RiLinkedinLine /></a>
+        <a href="https://github.com/AidanFournier" target="_blank" rel="noreferrer"><FiGithub /></a>
+        <a href="https://twitter.com/aidoufou" target="_blank" rel="noreferrer"><FiTwitter /></a> 
       </div>
       <div className="footer__copyright">
         <small>© Aidan Fournier 2022</small>
