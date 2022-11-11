e84a967d6c3cc766f856120d5a45160aa915a03e
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-11-11 22:33:34 +0900

    Added data to projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -21,28 +21,48 @@ const data = [
     image: JusticeImg,
     title: 'Justice',
     github: "https://github.com/AidanFournier/justice",
-    site: "https://www.wearejustice.life/"
+    site: "https://www.wearejustice.life/",
+    content: {
+      stack: 'Ruby on Rails',
+      description: 'Evidence-collecting app for victims of abuse. Users can document incidents, generate police reports in Japanese, and connect with other users who have the same abuser using facial-recogntion technology.',
+      role: 'As the lead Front-End Developer for this project, I had to think about how to design the app with mobile-first function in mind. I challenged myself by learning how to use Stimulus.js and create wireframes on Figma.'
+    }
   },
   {
     id: 2,
     image: CampQImg,
     title: 'CampQ',
     github: "https://github.com/AidanFournier/CampQ",
-    site: "https://campq.herokuapp.com/"
+    site: "https://campq.herokuapp.com/",
+    content: {
+      stack: 'Ruby on Rails',
+      description: 'A marketplace app for renting or lending camping gear. My team built this MVC app in two weeks. We used the Cloudinary and Mapbox APIs as well as the JS plugin Flatpickr.',
+      role: 'As the Project Manager, I learned invaluable skills in organizing my team and communicating with them using platforms such as Slack, Trello, and Github.'
+    }
   },
   {
     id: 3,
     image: NookPhoneImg,
     title: 'NookPhone',
     github: "https://github.com/AidanFournier/acnh-nook-phone",
-    site: "https://acnh-nookphone.com/"
+    site: "https://acnh-nookphone.com/",
+    content: {
+      stack: 'React',
+      description: 'This mobile-first application provides gameplay support for Animal Crossing New Horizons.',
+      role: 'This is my first independent project built with React.js. Along with using hooks for State and Effect, I challenged myself by using a library called Axios to support the API calls in the app.'
+    }
   },
   {
     id: 4,
     image: WatchListImg,
     title: 'WatchList',
     github: "https://github.com/AidanFournier/justice",
-    site: "https://www.wearejustice.life/"
+    site: "https://www.wearejustice.life/",
+    content: {
+      stack: 'Ruby on Rails',
+      description: 'Create lists of your favourite movies and share them with your friends.',
+      role: 'This project taught me how to host my site on Heroku and host images on Cloudinary.'
+    }
   }
 ]
 
