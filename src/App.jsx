8b9720dd2bc2f2c0ca6324a6fe85896ee392c1f6
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-11-27 23:45:05 +0900

    Made additions and corrections to metadata

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Helmet } from 'react-helmet-async'
-import { HelmetProvider } from 'react-helmet-async'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import Header from './components/Header'
 import Nav from './components/Nav'
 import About from './components/About'
@@ -15,12 +14,22 @@ const App = () => {
       <Helmet>
         <meta charset="UTF-8" />
         <title>Aidan Fournier</title>
+        <meta name="title" content="Aidan Fournier | Fullstack Developer" />
+        <meta name='description' content="Find out a little about me and my recent projects." />
+
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="Aidan Fournier | Fullstack Developer" />
         <meta property="og:description" content="Find out a little about me and my recent projects." />
         <meta property="og:image" content="./assets/SocialMediaCard.png" />
         <meta property="og:url" content="https://aidanfournier.com/" />
+        <meta property="og:site_name" content="Aidan Fournier | Fullstack Developer" />
+
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="og:site_name" content="Aidan Fournier | Fullstack Developer" ></meta>
+        <meta property="twitter:url" content="https://aidanfournier.com/" />
+        <meta property="twitter:creator" content="Aidan Fournier" />
+        <meta property="twitter:title" content="Aidan Fournier | Fullstack Developer" />
+        <meta property="twitter:description" content="Find out a little about me and my recent projects." />
+        <meta property="twitter:image" content="./assets/SocialMediaCard.png" />
       </Helmet>
       <Nav />
       <Header />
